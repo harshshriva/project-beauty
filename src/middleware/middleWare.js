6de539d7f97b96ce8fb233974d5d9747b364dcb5
Harsh Shrivastav <101973697+harshshriva@users.noreply.github.com>
2022-08-25 11:15:06 +0530

    Update middleWare.js

--- src/middleware/middleWare.js
+++ src/middleware/middleWare.js
@@ -1,16 +1,16 @@
-// const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 
-// const auth = async function(req, res, next) {
-//     const token = req.headers['x-api-key']
-//         // if (!token) return res.status(400).send({ msg: "please provide token" })
-//     const validToken = jwt.verify(token, 'projectOne')
+const auth = async function(req, res, next) {
+    const token = req.headers['x-api-key']
+        // if (!token) return res.status(400).send({ msg: "please provide token" })
+    const validToken = jwt.verify(token, 'projectOne')
 
-//     if (!validToken) {
-//         res.status(400).send({ status: false, msg: "user not found" })
-//     }
-//     //console.log(validToken)
-//     req.body.tokenId = validToken._id
-//     next()
-// }
-// module.exports.auth = auth;
-// const otpGenerator = require('otp-generator');
+    if (!validToken) {
+        res.status(400).send({ status: false, msg: "user not found" })
+    }
+    //console.log(validToken)
+    req.body.tokenId = validToken._id
+    next()
+}
+module.exports.auth = auth;
+const otpGenerator = require('otp-generator');
